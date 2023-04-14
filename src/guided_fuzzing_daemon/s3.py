b264d751573cbcd36dc1fc08856ecc17f7b32cf2
Jesse Schwartzentruber <truber@mozilla.com>
2023-04-14 10:08:52 -0400

    Add license headers

--- src/guided_fuzzing_daemon/s3.py
+++ src/guided_fuzzing_daemon/s3.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 S3Manager -- Class to manage builds, corpus and queues for AFL and libFuzzer on AWS S3
 
